727a23f1aec2f425a7ab841599fe15797fdbe644
Berncat <b.cattigan1@hotmail.com>
2022-10-30 03:40:48 +0000

    Setup

--- app/src/main/java/ie/wit/studentshareireland/activities/Create.kt
+++ app/src/main/java/ie/wit/studentshareireland/activities/Create.kt
@@ -1,11 +1,12 @@
-package ie.wit.studentshareireland
+package ie.wit.studentshareireland.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.wit.studentshareireland.R
 
-class MainActivity : AppCompatActivity() {
+class Create : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_create)
     }
 }
